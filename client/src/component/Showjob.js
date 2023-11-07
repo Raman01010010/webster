c40ccd06d368d3448bac66ff664ced64fd1ec037
Vivek9744 <125950773+Vivek9744@users.noreply.github.com>
2023-11-07 23:35:42 +0530

    ui and backend done for show job

--- client/src/component/Showjob.js
+++ client/src/component/Showjob.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Card, CardContent, Typography, Button } from '@mui/material';
-import 'tailwindcss/tailwind.css';
+import { Card, CardContent, Typography, Button, Box, Chip, Divider, Paper } from '@mui/material';
 import axios from "../api/axios";
+import {Link } from 'react-router-dom';
 
 const Showjob = () => {
   const [jobData, setJobData] = useState([]);
@@ -17,47 +17,73 @@ const Showjob = () => {
     };
     fetchData();
   }, []);
+
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex flex-col items-center justify-center">
       {jobData.map((job, index) => (
-        <Card key={index} className="w-80 mx-auto p-4 border border-gray-200 rounded-lg shadow-md">
+        <Card key={index} sx={{
+          maxWidth: 400,
+          margin: '16px',
+          transition: 'transform 0.2s',
+          '&:hover': {
+            transform: 'scale(1.05)',
+            boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+          },
+          backgroundColor: '#F0F6F1', // Add your desired background color here
+        }}>
           <CardContent>
-            <Typography variant="h6" className="font-semibold">
-              {job.titles}
+            <Typography variant="h5" component="div" sx={{ fontWeight: 'bold', color: '#1976D2', marginBottom: '8px' }}>
+              <span style={{ fontSize: '1.5rem', color: '#333' }}>{job.company}</span>
             </Typography>
-            <Typography variant="subtitle1" className="text-gray-500">
-              {job.company} - {job.locationonsite}
+            <Divider sx={{ margin: '8px 0' }} />
+            <Typography variant="h6" sx={{ color: '#333', marginBottom: '8px' }}>
+              Post Required: {job.titles}
             </Typography>
-            <Typography variant="body2" className="mt-2">
+            <Typography variant="subtitle1" sx={{ color: '#777', marginBottom: '8px' }}>
+              {job.locationonsite}
+            </Typography>
+            <Typography variant="body1" sx={{ color: '#333', marginBottom: '16px' }}>
               {job.details}
             </Typography>
-            <Typography variant="body2" className="mt-2">
+            <Divider sx={{ margin: '16px 0' }} />
+            <Typography variant="body1" sx={{ color: '#555', marginBottom: '8px' }}>
               <strong>Job Type:</strong> {job.jobtype}
             </Typography>
-            <Typography variant="body2" className="mt-2">
-              <strong>Skills Required:</strong> {job.skill.join(', ')}
-            </Typography>
-            <Typography variant="body2" className="mt-2">
+            <Box sx={{ display: 'flex', flexWrap: 'wrap', marginBottom: '16px' }}>
+              {job.skill.map((skill, i) => (
+                <Chip key={i} label={skill} sx={{ margin: '2px', backgroundColor: '#1976D2', color: 'white' }} />
+              ))}
+            </Box>
+            <Divider sx={{ margin: '16px 0' }} />
+            <Typography variant="body1" sx={{ color: '#555', marginBottom: '8px' }}>
               <strong>Contact:</strong>
-              <br />
+            </Typography>
+            <Typography variant="body1" sx={{ color: '#555' }}>
               Mobile: {job.contact[0]}
-              <br />
+            </Typography>
+            <Typography variant="body1" sx={{ color: '#555' }}>
               Email: {job.contact[1]}
-              <br />
+            </Typography>
+            <Typography variant="body1" sx={{ color: '#555' }}>
               Website: {job.contact[2]}
             </Typography>
-            <Typography variant="body2" className="mt-2">
+            <Divider sx={{ margin: '16px 0' }} />
+            <Typography variant="body1" sx={{ color: '#555', marginBottom: '8px' }}>
               <strong>Application Deadline:</strong> {job.lastdate}
             </Typography>
+            <Link to="/job">
             <Button
               variant="contained"
               color="primary"
               href={job.applylink}
+              
               target="_blank"
-              className="mt-4"
+              sx={{ mt: 2, backgroundColor: '#1976D2', color: 'white', fontWeight: 'bold' }}
+              
             >
               Apply Now
             </Button>
+            </Link>
           </CardContent>
         </Card>
       ))}
