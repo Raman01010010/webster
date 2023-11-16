c7f6e3dc03d1798644a0f679cb1dda1ba8c9756e
Raman01010010 <rmnprjrrr@gmail.com>
2023-11-16 19:47:25 +0530

    ui fix

--- client/src/component/NotifiButton.js
+++ client/src/component/NotifiButton.js
@@ -23,7 +23,11 @@ useEffect(() => {
 
   fetchNotifications();
 }, []); // The empty dependency array ensures that this effect runs once when the component mounts
-
+function fun(){
+  setSh(old=>{
+    return(!old)
+  })
+}
   return (<>
     <>
       {/* component */}
@@ -58,7 +62,7 @@ useEffect(() => {
                 role="button"
                 aria-label="close modal"
                 className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 rounded-md cursor-pointer"
-                onclick="notificationHandler(false)"
+                onClick={fun}
               >
                 <svg
                   width={24}
