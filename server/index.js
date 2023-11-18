9ec4885151d36135e97c3328d60faf5dad183a6a
Raman01010010 <rmnprjrrr@gmail.com>
2023-11-18 18:52:43 +0530

    minor fix

--- server/index.js
+++ server/index.js
@@ -73,8 +73,9 @@ app.use('/fetchendorse',require('./routes/fetchingEndorse'))
 app.use('/getpost',require('./routes/getpost'))
 
 app.use('/notif',require('./routes/notif'))
-app.use('/post',require('./routes/post'))
 app.use(verifyJWT)
+app.use('/post',require('./routes/post'))
+
 
 
 
